f36d3d5ca22390324553b70c91c0ae1b899b3039
Jean De Oliveira <96274161+JeanDeOliveira@users.noreply.github.com>
2022-01-21 07:41:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+let sec = 0
+let min = 0
+let hour = 0
+let OutputHours = document.querySelector('.hours')
+let OutputMins = document.querySelector('.mins')
+let OutputSecs = document.querySelector('.secs')
+
+function twoDigits(digit){
+	if(digit < 10){
+		return('0'+digit)
+	}else{
+		return(digit)
+	}
+}
+
+function Start(){
+	interval = setInterval(time, 1000)
+}
+
+function time(){
+	sec++
+	OutputSecs.innerHTML = twoDigits(sec)
+
+	if(sec==60){
+		tens=0
+		sec=0
+		min++
+
+		OutputMins.innerHTML = twoDigits(min)
+		if(min==60){
+			sec=0
+			min=0
+			hour++
+
+			OutputHours.innerHTML = twoDigits(hour)
+		}
+	}
+}
+
+function Pause(){
+	clearInterval(interval)
+}
+
+function Reset(){
+	sec=0
+	min=0
+	hour=0
+	OutputHours.innerText = '00'
+	OutputMins.innerText = '00'
+	OutputSecs.innerText = '00'
+	clearInterval(interval)
+}
